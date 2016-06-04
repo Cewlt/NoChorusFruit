1632edd44dbba3c3ff35e812f917dc31fbf55cb5
Colt <Cewlt@users.noreply.github.com>
2016-06-03 22:01:22 -0400

    Create NoChorusFruit.java

---
+++ colt/nochorusfruit/NoChorusFruit.java
@@ -0,0 +1,35 @@
+package colt.nochorusfruit;
+
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.base.Strings;
+
+public class NoChorusFruit extends JavaPlugin implements Listener {
+
+	String blockedMessage = "";
+
+	@Override
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+		blockedMessage = ChatColor.translateAlternateColorCodes('&',
+				getConfig().getString("blocked-message"));
+	}
+
+	@EventHandler
+	public void playerTeleportEvent(PlayerTeleportEvent event) {
+		if (event.getCause() == PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT) {
+			if (!event.getPlayer().hasPermission("ncf.bypass")) {
+				event.setCancelled(true);
+				if(!Strings.isNullOrEmpty(blockedMessage)) 
+					event.getPlayer().sendMessage(blockedMessage);
+			}
+		}
+	}
+}
